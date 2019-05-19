6b562f6867d018916a72f50f60c01d5bb79d4e2f
Sebastian Carroll <seabass25@hotmail.com>
2019-05-19 15:17:26 +0100

    Convert stings to points

--- polygon/polygon-perimeter/src/polygon_perimeter/core.clj
+++ polygon/polygon-perimeter/src/polygon_perimeter/core.clj
@@ -1,12 +1,19 @@
 (ns polygon-perimeter.core
   (:gen-class))
 
+(require '[clojure.string :as str])
+
 (defn distance [p1 p2] 
   (let [[p1x p1y] p1
         [p2x p2y] p2
         dx (- p2x p1x)
         dy (- p2y p1y)]
     (Math/sqrt (+ (* dx dx) (* dy dy)))))
+
+(defn str-to-points [str-line]
+  (map #(Integer/parseInt %) (str/split str-line #" ")))
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
--- polygon/polygon-perimeter/test/polygon_perimeter/core_test.clj
+++ polygon/polygon-perimeter/test/polygon_perimeter/core_test.clj
@@ -6,11 +6,15 @@
   (let [diff (Math/abs (- x y))]
     (< diff tolerance)))
 
-(deftest a-test
+(deftest distance-function
   (testing "Distance between points"
     (is (= 1.0 (distance [0 1] [0 0])))
     (is (= 2.0 (distance [0 0] [2 0])))
     (is (= 0.0 (distance [1 1] [1 1])))
     ; (sqrt 4 + 4) = 2.8284
-    (is (fuzzy= 0.0001 2.8284 (distance [0 0] [2 2])))
-    ))
+    (is (fuzzy= 0.0001 2.8284 (distance [0 0] [2 2])))))
+
+(deftest str-to-points-test
+  (testing "converting strings from stdin to points"
+    (is (= [0 0] (str-to-points "0 0")))
+    (is (= [0 2] (str-to-points "0 2")))))
