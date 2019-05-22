4584c64d5a761c06caddd6a915279d3bc1c0cc37
Sebastian Carroll <seabass25@hotmail.com>
2019-05-22 12:24:20 +0100

    Added new project for plygon area
    Too difficult for now to get the sharing working

---
+++ polygon/polygon-area/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to polygon-area
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ polygon/polygon-area/test/polygon_area/core_test.clj
@@ -0,0 +1,7 @@
+(ns polygon-area.core-test
+  (:require [clojure.test :refer :all]
+            [polygon-area.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
