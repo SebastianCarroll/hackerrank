e7bc761f6537e7a37df62c5aae303900fd89edbd
Sebastian Carroll <seabass25@hotmail.com>
2019-07-09 21:39:25 +0100

    Added initial pass at area program

--- polygon/polygon-area/src/polygon_area/core.clj
+++ polygon/polygon-area/src/polygon_area/core.clj
@@ -1,7 +1,39 @@
 (ns polygon-area.core
   (:gen-class))
 
+(require '[clojure.string :as str])
+
+; Input format
+; 4
+; 0 0
+; 0 1
+; 1 1
+; 1 0
+
+(defn map-to-ints [str-array] 
+  (map #(Integer/parseInt %) str-array))
+
+(defn sum-alternate-points [x y] 
+  (let [[x1 x2] x
+        [y1 y2] y]
+    [(* x1 y2) (* y1 x2)]))
+
+(defn sum-points [points])
+
+(defn sum-many-points [points]
+  (let [pairs (partition 2 1 points)
+        [xsum ysum ] ]))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  ;; readline and convert to int
+  ; range over that and map readline onto it
+  ; split convert each of those strings to int pair
+  ; reduce over those pairs and add up x1*y2, y1*x2
+  ; multiply and divide
+  (let [num-points (Integer/parseInt (read-line))
+        str-points (repeatedly num-points #(str/split (read-line) #" "))
+        int-points (map map-to-ints str-points)
+        ]
+  (println int-points) ))
--- polygon/polygon-area/test/polygon_area/core_test.clj
+++ polygon/polygon-area/test/polygon_area/core_test.clj
@@ -3,5 +3,14 @@
             [polygon-area.core :refer :all]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Summing Alternate points"
+    (is (= [4 6] (sum-alternate-points [1 2] [3 4])))
+    (is (= [4 6] (reduce sum-alternate-points [[1 2] [3 4]])))
+    (is (= [42 38] (reduce sum-alternate-points [[1 2][5 7][4 6]])))))
+
+; 1 2
+; 5 7
+; 4 6
+
+;7+(5*6)=7+36=42
+;(2*5)+(7*4)= 10+28=38
